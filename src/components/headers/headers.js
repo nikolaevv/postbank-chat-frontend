72bf08fb4ca4404b349b47c1dd6028fb893f204a
nikolaevv <starlei165@gmail.com>
2021-09-16 16:22:43 +0300

    feat: Create admin header

--- src/components/headers/headers.js
+++ src/components/headers/headers.js
@@ -1,7 +1,34 @@
 import React from 'react';
 
+import logo from '../../files/images/logo.png';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import './headers.css';
+import { Typography } from '@material-ui/core';
 
-const Headers = () => {};
+const BasicHeader = ({image, title}) => {
+    return (
+        <header>
+            <div className="header-image-box">
+                {image}
+            </div>
 
-export default Headers;
+            <div className="header-title">
+                <Typography variant="h4">
+                    {title}
+                </Typography>
+            </div>
+        </header>
+    );
+};
+
+const icon = <AccountCircleIcon fontSize="large" className="message-sender-icon"/>;
+
+const AdminHeader = () => {
+    return <BasicHeader image={icon} title={"Никита Любимов"}/>
+};
+
+const UserHeader = () => {
+    return <BasicHeader image={logo} title={"Почта Банк"}/>
+};
+
+export {AdminHeader, UserHeader};
--- src/components/pages/admin-page.js
+++ src/components/pages/admin-page.js
@@ -2,6 +2,7 @@ import { Container } from '@material-ui/core';
 import React from 'react';
 import MessageBoard from '../message-board/message-board';
 import Dialogs from '../dialogs/dialogs';
+import { AdminHeader } from '../headers/headers';
 
 import './admin-page.css';
 
@@ -9,6 +10,8 @@ const AdminPage = () => {
     return (
         <div>
             <Container>
+                <AdminHeader/>
+                
                 <div className="admin-board">
                     <Dialogs/>
                     <MessageBoard role="BANK"/>
