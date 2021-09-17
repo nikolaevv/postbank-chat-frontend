d8387e47a2fa13a3602646116e641c029ade08a7
nikolaevv <starlei165@gmail.com>
2021-09-17 18:43:17 +0300

    refactor: Add redirect from main URI

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Route, Redirect } from 'react-router';
 
 import AdminPage from '../pages/admin-page';
 import UserPage from '../pages/user-page';
@@ -9,6 +9,11 @@ import './app.css';
 const App = () => {
     return (
         <div>
+            <Route exact path="/">
+                <Redirect to="/support"/>
+            </Route>
+
+
             <Route path="/admin/support" exact component={AdminPage}/>
             <Route path="/support" exact component={UserPage}/>
         </div>
